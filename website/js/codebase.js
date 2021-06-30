9b0a75cd32638ba4e434ee8b2d485c889294f6f5
Aman Tandon <aman.tandon3096@gmail.com>
2021-06-30 13:50:30 +0530

    refactor: code generic code and responsive code

--- website/js/codebase.js
+++ website/js/codebase.js
@@ -1,5 +1,8 @@
-let firstImg = document.getElementById('first-img')
-let secondDiv = document.getElementById('second-div');
+let list = document.getElementsByTagName('img')
+let firstImg =list[1];
+let secondImg=list[2];
+
+
 let rim = document.createElement('iframe');
 let rim2 = document.createElement('iframe');
 
@@ -9,44 +12,42 @@ function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
+function defineAdStyle( element ) {
+    let reform = element;
+    reform.style.width= '100%',
+    reform.style.height = '90px',
+    reform.style.marginTop='-12%'
+    reform.style.backgroundColor = 'red'
+    reform.style.border = 'none'
+    return reform;
+}
 document.addEventListener('DOMContentLoaded', function() {
     div.style.width = '728px';
     div.style.height = '90px';
     div.id = 'second-inner-div';
+    rim = defineAdStyle(rim);
     rim.setAttribute('id','rim');
     rim.id='inner-rim'
     div.appendChild(rim)
-    rim.style.width = '728px'
-    rim.style.height = '90px'
-    rim.style.backgroundColor = 'red'
-    rim.style.border='none'
-    rim.style.marginTop='-90px'
-    
-    rim2.style.width = '100%'
-    rim2.style.height = '90px'
-    rim2.style.backgroundColor = 'red'
+    rim2 = defineAdStyle(rim2);
     rim2.id='inner-rimj'
-    rim2.style.border = 'none'
-    rim2.style.marginTop='-90px'
-
+    rim2.style.position='absolute'
     insertAfter(firstImg, rim2);
-    secondDiv.appendChild(div);
+    insertAfter(secondImg, rim);
 }, false);
 
 focus();
 
 
 var clickIframe = window.setInterval(checkFocus, 100);
-var i = 0;
 
 function checkFocus() {
   if(document.activeElement == document.getElementById('inner-rim')) {
     rim.style.backgroundColor = 'blue'
   	window.focus();
    }
-
    if(document.activeElement === document.getElementById('inner-rimj')){
     rim2.style.backgroundColor = 'blue'
     window.focus();
-}
+   }
 }
